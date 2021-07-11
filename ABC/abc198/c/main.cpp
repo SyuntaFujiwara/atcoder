9e48156f7a41f89646190181cd2b092cd9562fa7
SyuntaFujiwara <syunta.fujiwara.june30@gmail.com>
2021-07-11 21:27:44 +0900

    no message

---
+++ ABC/abc198/c/main.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+#define rep(i,n) for(int i=0;i<n;i++)
+using namespace std;
+using ll = long long;
+using p = pair<int,int>;
+using Graph = vector<vector<int>>;
+template<class T>bool chmax(T &a, const T &b) { if (a<b) { a=b; return 1; } return 0; }
+template<class T>bool chmin(T &a, const T &b) { if (b<a) { a=b; return 1; } return 0; }
+int main(int argc, const char * argv[]) {	
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+    cout<<fixed<<setprecision(15);
+
+    double r,x,y;
+    cin>>r>>x>>y;
+    double len = sqrt(pow(x,2)+pow(y,2));
+    int ans = len / r;
+    if(len != ans * r) {
+        ans++;
+    }
+    if(ans == 1 && len != r){
+        ans++;
+    }
+    cout<<ans<<endl;
+
+    return 0;
+}
+
